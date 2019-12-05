5b40c861884897334de8c0e1c3b5179aa0640920
Daniel Loibl <daniel.loibl@gmail.com>
2019-12-05 21:05:46 +0100

    little refactoring of day5

--- Day5/index.js
+++ Day5/index.js
@@ -9,42 +9,34 @@ function intCodeComputer(input, program = [3, 0, 4, 0, 99]) {
   let output = [];
   while (index < program.length) {
     const { opcode, parameters } = parseOpcode(program[index]);
-    switch (opcode) {
-      case 1:
-        opcode1(index, program, parameters);
-        index += 4;
-        break;
-      case 2:
-        opcode2(index, program, parameters);
-        index += 4;
-        break;
-      case 3:
-        opcode3(input)(index, program, parameters);
-        index += 2;
-        break;
-      case 4:
-        opcode4(output)(index, program, parameters);
-        index += 2;
-        break;
-      case 5:
-        index = opcode5(index, program, parameters);
-        break;
-      case 6:
-        index = opcode6(index, program, parameters);
-        break;
-      case 7:
-        opcode7(index, program, parameters);
-        index += 4;
-        break;
-      case 8:
-        opcode8(index, program, parameters);
-        index += 4;
-        break;
-      case 99:
-        return { program, output };
-      default:
-        return -1;
+    index = executeOpcode(opcode, input, output)(index, program, parameters);
+    if (index === -1) {
+      return { output, program };
     }
+  }
+}
+
+function executeOpcode(code, input, output) {
+  switch (code) {
+    case 1:
+      return opcode1;
+    case 2:
+      return opcode2;
+    case 3:
+      return opcode3(input);
+    case 4:
+      return opcode4(output);
+    case 5:
+      return opcode5;
+    case 6:
+      return opcode6;
+    case 7:
+      return opcode7;
+    case 8:
+      return opcode8;
+    case 99:
+    default:
+      return -1;
   }
 }
 
@@ -74,30 +66,33 @@ function opcodeArithmetic(operation) {
       readValue(index + 2, program, parameter[1])
     );
     writeValue(result)(index + 3, program, parameter[2]);
+    return index + 4;
   };
 }
 const opcodeJump = cond => (index, program, parameter) =>
   cond(readValue(index + 1, program, parameter[0]))
     ? readValue(index + 2, program, parameter[1])
     : index + 3;
+
 const opcodeCompare = compare => (index, program, parameter) => {
   const first = readValue(index + 1, program, parameter[0]);
   const second = readValue(index + 2, program, parameter[1]);
   writeValue(compare(first, second) ? 1 : 0)(index + 3, program, parameter[2]);
+  return index + 4;
 };
 
 const opcode1 = opcodeArithmetic((a, b) => a + b);
 const opcode2 = opcodeArithmetic((a, b) => a * b);
 const opcode3 = input => (index, program, parameter) =>
-  writeValue(input)(index + 1, program, parameter[0]);
+  writeValue(input)(index + 1, program, parameter[0]) && index + 2;
 const opcode4 = output => (index, program, parameter) =>
-  output.push(readValue(index + 1, program, parameter[0])) && program;
+  output.push(readValue(index + 1, program, parameter[0])) && index + 2;
 const opcode5 = opcodeJump(value => value !== 0);
 const opcode6 = opcodeJump(value => value === 0);
 const opcode7 = opcodeCompare((first, second) => first < second);
 const opcode8 = opcodeCompare((first, second) => first === second);
 
-console.log(intCodeComputer(0, SAMPLE_PROGRAM));
+console.log(intCodeComputer(1, SAMPLE_PROGRAM.slice()).output);
 
 // PART 2
-console.log(intCodeComputer(5, SAMPLE_PROGRAM));
+console.log(intCodeComputer(5, SAMPLE_PROGRAM.slice()).output);
