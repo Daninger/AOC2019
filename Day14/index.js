2344e0fbbc637fc3a5602e06652fab283c76cdea
Daniel Loibl <daniel.loibl@gmail.com>
2019-12-14 20:44:32 +0100

    Day14

--- Day14/index.js
+++ Day14/index.js
@@ -1,5 +1,5 @@
 const SAMPLE = require("fs")
-  .readFileSync("./Day14/data.txt")
+  .readFileSync("./Day14/input.txt")
   .toString()
   .split("\n")
   .map(parse);
@@ -19,13 +19,40 @@ function parse(equation) {
   return { input, output };
 }
 
-function produce(equations = SAMPLE, ingredient = "FUEL") {
+
+let stock = {};
+let ore = 0;
+
+function produce(equations = SAMPLE, ingredient = "FUEL", toProduce = 1) {
+  console.log(ingredient, toProduce, stock);
+  if (stock[ingredient] && stock[ingredient] > 0) {
+    toProduce -= stock[ingredient];
+    stock[ingredient] = 0;
+  }
   const formula = equations.find(e => e.output.ingredient === ingredient);
   if (!formula) {
-    return equations;
+    return stock;
   }
-  formula.input.forEach(v => (v.value *= formula.output.value));
-  formula.input.forEach(v => produce(equations, v.ingredient));
+  const factor = multiplier(toProduce, formula.output.value);
+  const rest = formula.output.value * factor - toProduce;
+  if (rest < 0) {
+    throw Error("Oops we did something wrong");
+  }
+  stock[formula.output.ingredient] =
+    (stock[formula.output.ingredient] || 0) + rest;
+  const oreFormula = formula.input.find(it => it.ingredient === "ORE");
+  if (oreFormula) {
+    ore += factor * oreFormula.value;
+  }
+
+  formula.input.forEach(v =>
+    produce(equations, v.ingredient, v.value * factor)
+  );
+  return ore;
+}
+
+function multiplier(toProduce, minimumProductionOutput) {
+  return Math.ceil(toProduce / minimumProductionOutput);
 }
 
 console.log(produce());
